2ecb5b66e362376ea7852182e7f3a057dece93c5
Filipe Rodrigues <subrodrigues@gmail.com>
2019-06-19 14:21:36 +0100

    [new] Helper method: warningMessageAlert(Stage stage, String header, String content, String actionButtonText)

--- src/main/java/utils/WidgetUtils.java
+++ src/main/java/utils/WidgetUtils.java
@@ -4,8 +4,14 @@
 
 package utils;
 
+import com.jfoenix.controls.JFXAlert;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialogLayout;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class WidgetUtils {
 
@@ -15,6 +21,25 @@ public class WidgetUtils {
         tt.getStyleClass().add("tooltip-style");
 
         Tooltip.install(obj, tt);
+    }
+
+    public static void warningMessageAlert(Stage stage, String header, String content, String actionButtonText){
+        JFXAlert alert = new JFXAlert(stage);
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.setOverlayClose(false);
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setHeading(new Label(header));
+        layout.setBody(new Label(content));
+
+        JFXButton okButton = new JFXButton(actionButtonText);
+
+        okButton.getStyleClass().add("alert-accept");
+        okButton.setOnAction(event -> alert.hideWithAnimation());
+
+        layout.setActions(okButton);
+
+        alert.setContent(layout);
+        alert.show();
     }
 
 }
